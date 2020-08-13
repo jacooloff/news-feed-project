6655a0d54eaaba72155fddb3551dbcb3f27dbb64
jacooloff <alshynzhakulov@gmail.com>
2020-08-13 23:50:57 +0600

    fix(create/edit novelty): add imports

--- source/newsfeedproject/feed/views.py
+++ source/newsfeedproject/feed/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Novelty
+from .forms import NoveltyForm, NoveltyFormForEdit
 
 def feed(request):
     noveltys = Novelty.objects.all().order_by('published_date')
