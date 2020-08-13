59ba0a32e0d70e5ff4393738184110cdcac18d82
jacooloff <alshynzhakulov@gmail.com>
2020-08-13 23:34:15 +0600

    feat(detail): make novelty detail

--- source/newsfeedproject/feed/urls.py
+++ source/newsfeedproject/feed/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
                 path('', views.feed, name = "feed"),
+                path('novelty/<int:pk>/', views.novelty_detail, name = "novelty_detail"),
                 ]
--- source/newsfeedproject/feed/views.py
+++ source/newsfeedproject/feed/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Novelty
 
 def feed(request):
     noveltys = Novelty.objects.all().order_by('published_date')
     return render(request, 'feed/index.html', {'noveltys':noveltys})
+
+def novelty_detail(request, pk):
+    novelty = get_object_or_404(Novelty, pk=pk)
+    return render(request, 'feed/detail.html', {'novelty':novelty})
