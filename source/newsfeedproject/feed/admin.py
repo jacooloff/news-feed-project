352b225aa7e2f5d1be89f9c753ba741ceb0f6ed5
jacooloff <alshynzhakulov@gmail.com>
2020-08-13 21:10:01 +0600

    feat(python deps;model): reqs for db,server; make,migrate,add to admin novelty model

--- source/newsfeedproject/feed/admin.py
+++ source/newsfeedproject/feed/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Novelty
 
-# Register your models here.
+admin.site.register(Novelty)
--- source/newsfeedproject/feed/models.py
+++ source/newsfeedproject/feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Novelty(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
